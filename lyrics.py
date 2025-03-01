38e9b175637ac466c833f81b27416d488877f150
Floris Tabak <floristabak@gmail.com>
2025-03-01 16:36:52 +0100

    created look ahead and polished

--- lyrics.py
+++ lyrics.py
@@ -1,32 +1,34 @@
 #! /usr/bin/env python
 import subprocess as s
+import syncedlyrics as sy
 import threading as t
 from time import sleep
 import os, textwrap
 
-def waitfornextsong():
+def getsong():
     output = s.run(['cmus-remote', '-Q'], stdout=s.PIPE).stdout.decode('utf-8')
-    song = [k[2:] for k in [j.split() for j in [i for i in output.split('\n') if i.startswith("tag artist") or i.startswith("tag title")]]]
+    songraw = [k[2:] for k in [j.split() for j in [i for i in output.split('\n') if i.startswith("tag artist") or i.startswith("tag title")]]]
+    time = int([k[-1] for k in [j.split() for j in [i for i in output.split('\n') if i.startswith("position")]]][0])
+    return songraw, time
+
+def waitfornextsong():
+    song = getsong()[0]
     previousSong = song
     while song == previousSong:
         previoussong = song
-        output = s.run(['cmus-remote', '-Q'], stdout=s.PIPE).stdout.decode('utf-8')
-        song = [k[2:] for k in [j.split() for j in [i for i in output.split('\n') if i.startswith("tag artist") or i.startswith("tag title")]]]
+        song = getsong()[0]
     return
 
 def start():
-    output = s.run(['cmus-remote', '-Q'], stdout=s.PIPE).stdout.decode('utf-8')
-    songraw = [k[2:] for k in [j.split() for j in [i for i in output.split('\n') if i.startswith("tag artist") or i.startswith("tag title")]]]
-    time = int([k[-1] for k in [j.split() for j in [i for i in output.split('\n') if i.startswith("duration")]]][0])
+    songraw = getsong()[0]
     song = " ".join(songraw[1] + songraw[0])
     try:
         if not os.path.isfile(f"/home/Floris/.cache/lyricist/{song}.lrc"):
-            s.call(['syncedlyrics', f"{song}", '--synced-only'], stdout=open(os.devnull, "w"), stderr=open(os.devnull, "w"))
+            sy.search(song, synced_only=True, save_path=f"{song}.lrc", providers=["NetEase", "Lrclib", "Megalobiz"])
             os.system(f'mv "{song}.lrc" ~/.cache/lyricist/')
         with open(f"/home/Floris/.cache/lyricist/{song}.lrc", "r") as f:
             lyrics = [i.split() for i in f.read().split('\n')]
-    except:
-        os.system("clear")
+    except Exception as e:
         print(f'could not find any lyrics for "{song}"')
         waitfornextsong()
         return
@@ -34,10 +36,14 @@ def start():
     previoussong = songraw
     while 1:
         previoussong = songraw
-        info = s.run(['cmus-remote', '-Q'], stdout=s.PIPE).stdout.decode('utf-8')
-        currenttime = int([k[-1] for k in [j.split() for j in [i for i in info.split('\n') if i.startswith("position")]]][0])
-        songraw = [k[2:] for k in [j.split() for j in [i for i in info.split('\n') if i.startswith("tag artist") or i.startswith("tag title")]]]
-        if currenttime in timings: os.system("clear"); print(textwrap.fill(' '.join(lyrics[timings.index(currenttime)][1:]),40), end='\r'); sleep(1.2)
+        songraw, currenttime = getsong()
+        if currenttime in timings:
+            os.system("clear")
+            print(textwrap.fill(' '.join(lyrics[timings.index(currenttime)][1:]),40), end='\r')
+            print("\n")
+            if len(lyrics) > timings.index(currenttime)+1:
+                print(textwrap.fill(' '.join(lyrics[timings.index(currenttime)+1][1:]),40), end='\r')
+            sleep(1.2)
         if songraw != previoussong: return
 print('\033[?25l', end="")
 while 1: os.system("clear"); start()
